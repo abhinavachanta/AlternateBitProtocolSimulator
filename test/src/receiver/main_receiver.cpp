47f291e7d7092ea0e7e39d7468f2fc565b6d6bfd
abhinavachanta <53234950+abhinavachanta@users.noreply.github.com>
2019-08-06 22:34:08 -0400

    Update main_receiver.cpp

--- test/src/receiver/main_receiver.cpp
+++ test/src/receiver/main_receiver.cpp
@@ -34,8 +34,7 @@ template <typename T>
     {
         public: ApplicationGen() = default;
         ApplicationGen(const char *file_path): 
-			iestream_input <message_t, T> (file_path)
-        {}
+	iestream_input <message_t, T> (file_path) {}
     };
 
 int main() {
@@ -51,30 +50,30 @@ int main() {
     };
 
     using info = cadmium::logger::logger <cadmium::logger::logger_info,
-		                                  cadmium::dynamic::logger::formatter <TIME>, 
-										  oss_sink_provider> ;
+		                          cadmium::dynamic::logger::formatter <TIME>, 
+					  oss_sink_provider> ;
     using debug = cadmium::logger::logger <cadmium::logger::logger_debug,
-		                                   cadmium::dynamic::logger::formatter <TIME>, 
-										   oss_sink_provider> ;
+		                           cadmium::dynamic::logger::formatter <TIME>, 
+					   oss_sink_provider> ;
     using state = cadmium::logger::logger <cadmium::logger::logger_state,
-		                                   cadmium::dynamic::logger::formatter <TIME>, 
+		                           cadmium::dynamic::logger::formatter <TIME>, 
                                            oss_sink_provider> ;
     using log_messages = cadmium::logger::logger <cadmium::logger::logger_messages,
-												  cadmium::dynamic::logger::formatter <TIME>, 
-												  oss_sink_provider> ;
+						  cadmium::dynamic::logger::formatter <TIME>, 
+						  oss_sink_provider> ;
     using routing = cadmium::logger::logger<cadmium::logger::logger_message_routing,
-											cadmium::dynamic::logger::formatter <TIME>,
-											oss_sink_provider> ;
+					    cadmium::dynamic::logger::formatter <TIME>,
+					    oss_sink_provider> ;
     using global_time = cadmium::logger::logger<cadmium::logger::logger_global_time,
-												cadmium::dynamic::logger::formatter <TIME>, 
-												oss_sink_provider> ;
+						cadmium::dynamic::logger::formatter <TIME>, 
+						oss_sink_provider> ;
     using local_time = cadmium::logger::logger <cadmium::logger::logger_local_time,
-												cadmium::dynamic::logger::formatter <TIME>, 
-												oss_sink_provider> ;
+						cadmium::dynamic::logger::formatter <TIME>, 
+						oss_sink_provider> ;
     using log_all = cadmium::logger::multilogger <info, debug, state, log_messages,
-												  routing, global_time, local_time> ;
+						  routing, global_time, local_time> ;
     using logger_top = cadmium::logger::multilogger <log_messages,
-													 global_time> ;
+						     global_time> ;
 
     /*******************************************/
 
@@ -86,8 +85,8 @@ int main() {
 
     std::shared_ptr <cadmium::dynamic::modeling::model> generator = 
     cadmium::dynamic::translate::make_dynamic_atomic_model
-	< ApplicationGen, TIME, const char *> 
-	("generator", std::move(i_input_data_control));
+    < ApplicationGen, TIME, const char *> 
+    ("generator", std::move(i_input_data_control));
 
     /********************************************/
     /****** RECIEVER *******************/
@@ -102,7 +101,7 @@ int main() {
     /************************/
     cadmium::dynamic::modeling::Ports iports_TOP = {};
     cadmium::dynamic::modeling::Ports oports_TOP = {
-		typeid(output_port)
+	typeid(output_port)
     };
     cadmium::dynamic::modeling::Models submodels_TOP = {
         generator, receiver1
@@ -114,18 +113,18 @@ int main() {
     };
     cadmium::dynamic::modeling::ICs ics_TOP = {
         cadmium::dynamic::translate::make_IC <iestream_input_defs <message_t>::out, 
-		                                      receiver_defs:: in> ("generator", "receiver1")
+		                              receiver_defs:: in> ("generator", "receiver1")
     };
     std::shared_ptr <cadmium::dynamic::modeling::coupled <TIME>> TOP = 
     std::make_shared <cadmium::dynamic::modeling::coupled <TIME>> (
-																	"TOP",
-																	submodels_TOP,
-																	iports_TOP,
-																	oports_TOP,
-																	eics_TOP,
-																	eocs_TOP,
-																	ics_TOP
-																	);
+								   "TOP",
+								   submodels_TOP,
+							           iports_TOP,
+								   oports_TOP,
+								   eics_TOP,
+								   eocs_TOP,
+								   ics_TOP
+								   );
 
     ///****************////
 
